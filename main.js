a10a1afd16b1769686d03a07748c09ca8d6fde64
2emk7 <104653877+2emk7@users.noreply.github.com>
2022-12-12 22:48:49 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 console.clear();
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-process.setMaxListeners(0);
+const prompt = require('prompt-sync')();
 
+process.setMaxListeners(0);
 console.log(`
  _______  _______  _______  ___            _______  __   __  _______ 
 |       ||       ||   _   ||   |          |       ||  |_|  ||       |
@@ -14,7 +15,29 @@ console.log(`
                                      
 `);
 
+function runMe() {
+  const age = prompt(' run? (Y/N) ');
 
+  if(age == 'Y'){
+    console.clear();
+    console.log(`
+ _______  _______  _______  ___            _______  __   __  _______ 
+|       ||       ||   _   ||   |          |       ||  |_|  ||       |
+|       ||   _   ||  |_|  ||   |          |    ___||       ||    ___|
+|       ||  | |  ||       ||   |          |   |___ |       ||   |___ 
+|      _||  |_|  ||       ||   |___  ___  |    ___| |     | |    ___|
+|     |_ |       ||   _   ||       ||   | |   |___ |   _   ||   |___ 
+|_______||_______||__| |__||_______||___| |_______||__| |__||_______|
+                                     
+`);
+    run()
+  }
+  else{
+    console.log("stop");
+  }
+}
+
+async function run(){
 fs.readFile('C:/Users/Coach/AppData/Roaming/.minecraft/logs/blclient/minecraft/latest.log', 'utf8', async (err, data) => {
   if (err) throw err;
 
@@ -48,7 +71,7 @@ fs.readFile('C:/Users/Coach/AppData/Roaming/.minecraft/logs/blclient/minecraft/l
             console.log('\x1b[36m', values,'\x1b[0m' + 'This player has never played bedwars');
             console.log("---------------------------------------------");
           }
-      async function scrapeProduct(url) {
+          async function scrapeProduct(url) {
          
           const browser = await puppeteer.launch();
           const page = await browser.newPage();
@@ -76,9 +99,12 @@ fs.readFile('C:/Users/Coach/AppData/Roaming/.minecraft/logs/blclient/minecraft/l
           console.log("---------------------------------------------");
       
       }
-    }
-    console.log("-----------------------------------------------------------------------------------------------------------------------");
-    }
+    }  
+  }
   
+   runMe()
 });
 
+}
+
+ runMe()
