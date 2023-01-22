0975644e919444517b13061eeb0000c3b5775395
2emk7 <104653877+2emk7@users.noreply.github.com>
2023-01-21 23:51:55 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const prompt = require('prompt-sync')();
 const Hypixel = require('hypixel-api-reborn');
 
-const hypixel = new Hypixel.Client('ENTER API HERE');
+const hypixel = new Hypixel.Client('enterapihere');
 
 const filePath = 'C:/Users/Coach/AppData/Roaming/.minecraft/logs/blclient/minecraft/latest.log';
 const keyword = 'ONLINE: ';
@@ -44,68 +44,61 @@ const checkForUpdates = () => {
             const playerName = name.replace(keyword, '');
             try {
               const player = await hypixel.getPlayer(playerName);
+              const winstreak = player.stats.bedwars.winstreak || '?';
+              const finalKills = player.stats.bedwars.finalKills;
               const fkdr = player.stats.bedwars.finalKDRatio;
+              let wins = player.stats.bedwars.wins;
               let color;
+              let color2;
+
       
           if (fkdr > 50) {
-            color = '\x1b[95m'; // Pink
-          } else if (fkdr > 20) {
-            color = '\x1b[35m'; // Purple
-          } else if (fkdr > 10) {
-            color = '\x1b[34m'; // Blue
-          } else if (fkdr > 8) {
-            color = '\x1b[36m'; // Cyan
-          } else if (fkdr > 6) {
-            color = '\x1b[31m'; // Red
-          } else if (fkdr > 4) {
-            color = '\x1b[32m'; // Green
-          } else if (fkdr > 2) {
-            color = '\x1b[33m'; // Yellow
-          } else {
-            color = '\x1b[0m'; // Default (no color)
-          }
-
-
-          let wins = player.stats.bedwars.wins;
-          let color2;
-
-          if (wins > 15000) {
-            color2 = '\x1b[95m'; // Pink
-          } else if (wins > 10000) {
-            color2 = '\x1b[35m'; // Purple
-          } else if (wins > 8000) {
-            color2 = '\x1b[34m'; // Blue
-          } else if (wins >6000) {
-            color2 = '\x1b[36m'; // Cyan
-          } else if (wins > 4000) {
-            color2 = '\x1b[31m'; // Red
-          } else if (wins > 1500) {
-            color2 = '\x1b[32m'; // Green
-          } else if (wins > 500) {
-            color2 = '\x1b[33m'; // Yellow
-          } else {
-            color2 = '\x1b[0m'; // Default (no color)
-          }
-      
-          let lvl = player.stats.bedwars.level;
-
-          
-          console.log('\x1b[0m', playerName + '\x1b[0m' +"  LVL - " + lvl + '\x1b[0m' +"  WS - " + player.stats.bedwars.winstreak + '\x1b[0m' +"   FKDR - " + color, fkdr + '\x1b[0m' +"   WINS - " + color2, player.stats.bedwars.wins)
-          console.log('\x1b[0m' + "---------------------------------------")
-          } catch (err) {
-            var nick = playerName;
-            console.log(`${nick} is nicked`);
-            console.log('\x1b[0m' + "---------------------------------------")
-          }
+            color = '\x1b[95m'; //Pink
+        } else if (fkdr > 20) {
+        color = '\x1b[35m'; // Purple
+        } else if (fkdr > 10) {
+        color = '\x1b[34m'; // Blue
+        } else if (fkdr > 8) {
+        color = '\x1b[36m'; // Cyan
+        } else if (fkdr > 6) {
+        color = '\x1b[31m'; // Red
+        } else if (fkdr > 4) {
+        color = '\x1b[32m'; // Green
+        } else if (fkdr > 2) {
+        color = '\x1b[33m'; // Yellow
+        } else {
+        color = '\x1b[0m'; // Default (no color)
         }
-          )
+  
+        if (wins > 15000) {
+          color2 = '\x1b[95m'; // Pink
+        } else if (wins > 10000) {
+          color2 = '\x1b[35m'; // Purple
+        } else if (wins > 8000) {
+          color2 = '\x1b[34m'; // Blue
+        } else if (wins > 6000) {
+          color2 = '\x1b[36m'; // Cyan
+        } else if (wins > 4000) {
+          color2 = '\x1b[31m'; // Red
+        } else if (wins > 2000) {
+          color2 = '\x1b[32m'; // Green
+        } else if (wins > 1000) {
+          color2 = '\x1b[33m'; // Yellow
+        } else {
+          color2 = '\x1b[0m'; // Default (no color)
+        }
+        
+        
+        console.log(`${color}${playerName}: ws - ${winstreak} fkdr - ${fkdr} finals - ${finalKills} wins - ${color2}${wins}`);
+          } catch (err) {
+            console.error(`${playerName} Is Nicked`);
+          }
+        });
         lastTrimmed = trimmed;
       }
-      
     }
-    
-  });
-  
+  })
+  .catch(console.error);
 };
 
 setInterval(checkForUpdates, 1000);
